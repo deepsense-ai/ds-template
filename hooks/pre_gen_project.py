00f362a23ab103d56e86c51515b734b0d5de70b9
Jakub Cierocki <jakub.cierocki@deepsense.ai>
2024-07-29 18:09:50 +0200

    code review fixes - removed Python Semantic Release (it will be provided in another PR)

--- hooks/pre_gen_project.py
+++ hooks/pre_gen_project.py
@@ -17,11 +17,4 @@ if not re.match(valid_python_name, package_name):
     print(f"ERROR: '{package_name}' is not a valid Python module name.")
     sys.exit(1)
 
-# TODO temporary, until GitLab CI support for Semantic Release will be added
-ci_type = '{{ cookiecutter.ci }}'
-versioning_approach = '{{ cookiecutter.versioning }}'
-if ci_type == 'GitLab' and versioning_approach == "Python Semantic Release":
-    print(f"ERROR: 'Python Semantic Release' is currently not supported for projects with GitLab CI.")
-    sys.exit(1)
-
 print("Template looks ok.")
