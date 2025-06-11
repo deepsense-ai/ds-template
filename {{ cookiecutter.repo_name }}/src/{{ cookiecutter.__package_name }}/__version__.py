8448ba4290ae4355e33a6da7956fc292fb6b8cf3
Szymon Betlewski <szymon.betlewski@deepsense.ai>
2025-06-11 20:59:45 +0200

    Refactor version management to read from VERSION file

--- {{ cookiecutter.repo_name }}/src/{{ cookiecutter.__package_name }}/__version__.py
+++ {{ cookiecutter.repo_name }}/src/{{ cookiecutter.__package_name }}/__version__.py
@@ -1,3 +1,6 @@
 """Version information."""
 
-__version__ = "0.0.1-dev"
+from pathlib import Path
+
+version_file = Path(__file__).absolute().parents[0] / "VERSION"
+__version__ = version_file.read_text(encoding="utf-8").strip()
